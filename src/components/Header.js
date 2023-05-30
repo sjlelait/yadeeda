e38a8f969ee2b3a1a756e2a58e914156f383a2d5
Sara Le Lait <sjlelait@gmail.com>
2023-05-30 17:12:09 -0600

    wip: edits header for yadeeda visibility

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
 
     return (
         <header className="bg-white">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-6" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <Link to="/" className="-m-1.5 p-1.5">
                         <span className="sr-only">Yadeeda</span>
@@ -27,25 +27,25 @@ const Header = (props) => {
                         <Bars3Icon className="h-6 w-6" aria-hidden="true" />
                     </button>
                 </div>
-                <Popover.Group className="hidden lg:flex lg:gap-x-12">
+                <Popover.Group className="hidden lg:flex lg:gap-x-18">
                     <Popover className="relative">
-                        <Link to="/" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/" className="text-md font-semibold leading-6 text-gray-900">
                             <span className="sr-only">Home</span>
                             Home
                         </Link>
-                        <Link to="/music" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/music" className="text-md font-semibold leading-6 text-gray-900">
                             <span className="sr-only">Music & Videos</span>
                             Music & Videos
                         </Link>
-                        <Link to="/about" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/about" className="text-md font-semibold leading-6 text-gray-900">
                             <span className="sr-only">About Us</span>
                             About Us
                         </Link>
-                        <Link to="/links" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/links" className="text-md font-semibold leading-6 text-gray-900">
                             <span className="sr-only">Links We Like</span>
                             Links We Like
                         </Link>
-                        <Link to="/contact" className="text-sm font-semibold leading-6 text-gray-900">
+                        <Link to="/contact" className="text-md font-semibold leading-6 text-gray-900">
                             <span className="sr-only">Contact Us</span>
                             Contact Us
                         </Link>                    
@@ -55,6 +55,10 @@ const Header = (props) => {
                     <div className="fixed inset-0 z-10" />
                     <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
+                            <Link to="/" className="-m-1.5 p-1.5">
+                                <span className="sr-only">Yadeeda</span>
+                                Yadeeda
+                            </Link>
                             <button
                                 type="button"
                                 className="-m-2.5 rounded-md p-2.5 text-gray-700"
