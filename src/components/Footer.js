1079291abf006586e1990e74a85c16d03dcdb3aa
Sara Le Lait <sjlelait@gmail.com>
2023-06-03 09:35:09 -0600

    wip: edits footer for responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,17 @@ import { Link } from 'react-router-dom';
 function Footer() {
     return (
         <footer className="fixed bottom-0 left-0 right-0 w-full text-center justify-between">
-            <div className="flex flex-col lg:flex-row items-center justify-center p-6 dark:border-neutral-500 lg:justify-between">
-                <div className="mr-12 hidden lg:block">
-                    <span>Get connected with us on social networks:</span>
-                </div>
-                <div className="flex justify-center">
-                    <Link to="/" className="mr-6 text-neutral-600 dark:text-neutral-400">HOME</Link>
-                    <Link to="/about" className="mr-6 text-neutral-600 dark:text-neutral-400">ABOUT</Link>
-                    <Link to="/links" className="mr-6 text-neutral-600 dark:text-neutral-400">LINKS</Link>
-                    <Link to="/reviews" className="mr-6 text-neutral-600 dark:text-neutral-400">REVIEWS</Link>
-                    <Link to="/contact" className="mr-6 text-neutral-600 dark:text-neutral-400">CONTACT</Link>
-                </div>
-                <div className="text-center mt-4 lg:mt-0">
-                <p>© Yadeeda { new Date().getFullYear() }</p>
+            <div className="container mx-auto px-4 lg:px-8">
+                <div className="flex flex-col items-center justify-center p-6 dark:border-neutral-500 lg:justify-between">
+                    <div className="flex justify-center flex-wrap md-2">
+                        <Link to="/" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">HOME</Link>
+                        <Link to="/about" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">ABOUT</Link>
+                        <Link to="/reviews" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">REVIEWS</Link>
+                        <Link to="/contact" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">CONTACT</Link>
+                    </div>
+                    <div className="text-center mt-4 lg:mt-0">
+                        <p className="text-sm">© Yadeeda { new Date().getFullYear() }</p>
+                    </div>
                 </div>
             </div>
         </footer>
