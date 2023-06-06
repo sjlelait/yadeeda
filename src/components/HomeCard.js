08a7463ba55ce25af6e5b0cc1e73af3b941dc21c
Sara Le Lait <sjlelait@gmail.com>
2023-06-06 17:21:47 -0600

    wip: adds HomeCards

---
+++ src/components/HomeCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomeCard = ({ title, image, description, link, bgColor }) => {
+  return (
+    <div className={`rounded-lg shadow-lg p-4 ${bgColor}`} style={{ width: '300px' }}>
+      {image && (
+        <img src={image} alt={title} className="w-full h-32 object-cover mb-4 rounded" />
+      )}
+      {title && <h2 className="text-xl font-bold mb-2">{title}</h2>}
+      {description && <p>{description}</p>}
+      {link && 
+        <a href={link} className="bg-orange-500 text-white px-4 py-2 mt-4 inline-block rounded">
+          Read More
+        </a>}
+    </div>
+  );
+};
+
+export default HomeCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Card from '../components/HomeCard';
 
 const Home = (props) => {
+    const cardData = [
+        {
+          title: '10 Reasons to use our songs',
+          image: null,
+          description: '1. one of the best ways to introduce a new language',
+          link: 'https://example.com/card1',
+          bgColor: 'bg-red-500',
+        },
+        {
+          title: 'Video of the Day',
+          image: null,
+          description: 'Random Video Pulled from Youtube, embedded ',
+          link: 'https://www.youtube.com/@tialela99',
+          bgColor: 'bg-yellow-500',
+        },
+        {
+          title: 'Sesame Street',
+          image: null,
+          description: 'Yadeeda is on Sesame Street! Alain Le Lait\'s popular song "Sit Down, Stand Up" can now be heard on episode #4135 (The Bookaneers), the French version "Assis, Debout" is in episode #4151 (Slimey competes for the Worm Cup).',
+          link: 'https://example.com/card1',
+          bgColor: 'bg-green-500',
+        },
+      ];
+
     return (
-        <div className='home'>
-            <h1>Music and Videos by Alain Le Lait</h1>
+        <div className="flex">
+            {cardData.map((card, index) => (
+                <Card 
+                    key={index} 
+                    title={card.title} 
+                    image={card.image} 
+                    description={card.description}
+                    link={card.link}
+                    bgColor={card.bgColor}
+                />
+            ))}
         </div>
     )
 }
