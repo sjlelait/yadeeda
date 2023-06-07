298084b247cfa26870ab181e50e2549455ee21f7
Sara Le Lait <sjlelait@gmail.com>
2023-06-07 17:45:57 -0600

    wip: adds songdata 1-10

--- src/MusicData.js
+++ src/MusicData.js
@@ -1,8 +1,10 @@
 export const songs = [
     {
         id: 1,
-        titleE: `Ah, Les L\u00E9gumes`,
+        image: '',
+        titleE: `Ah, Les Légumes`,
         titleF: 'Ah, The Vegetables',
+        album: 'Parapluie',
         french: `Les haricots verts
         Les artichaux, les pommes de terre
         Poussent dans mon jardin
@@ -37,89 +39,439 @@ export const songs = [
         C'est délicieux et c'est trés sain
         Les légumes
         C'est trés bon et ça fait du bien`,
-        english: '',
+        english: `The green beans
+        The artichokes, the potatoes
+        Grow in my garden 
+        I water them every morning
+        
+        I have eggplants
+        Tomatoes and spinach
+        In my little garden
+        I water them every morning
+        
+        Ah, vegetables
+        They are delicious and they are very healthy
+        Ah, my vegetables
+        I water them every morning
+        
+        The cauliflowers and the zucchinis
+        Play tricks on each other
+        While the carrots
+        Underneath the dirt quarrel 
+        
+        Ah, the vegetables
+        They are delicious and they are very healthy
+        Ah, my vegetables
+        I water them every morning
+        
+         The green beans
+        The artichokes, the potatoes
+        Grow in my garden 
+        I water them every morning
+        
+        Vegetables
+        They are delicious and they are very healthy
+        Vegetables
+        They are very good and it does you good`,
         copyright: '© 2006 Alain Le Lait',
         link: '',
 
     },
     {
         id: 2,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: 'Arc-en-ciel',
+        titleF: 'Rainbow',
+        album: 'Parapluie',
+        french: `Il a les yeux rouges
+        Il a les yeux jaunes
+        Il a les yeux arc-en-ciel
+        Et une tête en bois
+        Il boit du chocolat
+        À tous les repas
+        Comme les poissons-chats
+        Cha la la la la
+        
+        Il a les yeux verts
+        Il a les yeux gris
+        Il a les yeux arc-en-ciel
+        Et une tête en bois
+        Il boit du jus de citron
+        Et de champignons
+        Comme Napoléon
+        On on on on on
+        
+        Il a les yeux blancs
+        Il a les yeux noirs
+        Il a les yeux arc-en-ciel
+        Et une tête en bois
+        Il boit la noix de coco
+        Avant de faire dodo
+        Do ré mi la si do
+        O o o o o
+        
+        Il a les yeux rouges
+        Il a les yeux jaunes
+        Il a les yeux arc-en-ciel
+        Et une tête en bois
+        Il boit du café au lait
+        Au lait, au lait, au lait
+        Et quand il a fini
+        Il se met au lit 
+        i i i i i 
+        o o o o o
+        Cha la la la la`,
+        english: `He has red eyes
+        He has yellow eyes
+        He has rainbow eyes
+        And a wooden head
+        He drinks hot chocolate
+        At every meal
+        Like the catfish
+        Cha la la la la
+        
+        He has green eyes
+        He has grey eyes
+        He has rainbow eyes
+        And a wooden head
+        He drinks lemon
+        And mushroom juice
+        Like Napoléon
+        On on on on on
+        
+        He has white eyes
+        He has black eyes
+        He has rainbow eyes
+        And a wooden head
+        He drinks the coconut
+        Before going to sleep
+        Do re mi la si do 
+        o o o o o
+        
+        He has red eyes
+        He has yellow eyes
+        He has rainbow eyes
+        And a wooden head
+        He drinks coffee with milk
+        With milk, with milk, with milk
+        And when he is done
+        He puts himself to bed
+        i i i i i 
+        o o o o o
+        Cha la la la la la`,
+        copyright: '© 2006 Alain Le Lait',
         link: '',
     },
     {
         id: 3,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: 'Big Nose',
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `It’s a dog, it’s a dog
+        Woof, woof, woof (x2)
+        It’s a dog, it’s a dog
+        With a great big nose
+        It’s a cat, it’s a cat
+        Meow, meow (x2)
+        It’s a cat, it’s a cat
+        With a great big nose
+        
+        A dog and a cat 
+        With a great big nose
+        This is quite a sight
+        Everybody knows 
+        (x2)
+        
+        It’s a bird, it’s a bird
+        - Whistle 3 times - (x2)
+        It’s a bird, it’s a bird
+        With a great big nose
+        It’s a fish, it’s a fish
+        - Smack lips 3 times - (x2)
+        It’s a fish, it’s a fish
+        With a great big nose
+        
+        A bird and a fish
+        With a great big nose
+        This is quite a sight
+        Everybody knows 
+        (x2)
+        
+        It’s a cow, it’s a cow
+        Moo, moo (x2)
+        It’s a cow, it’s a cow
+        With a great big nose
+        It’s a pig, it’s a pig
+        Oink, oink, oink (x2)
+        It’s a pig, it’s a pig
+        With a great big nose
+        
+        A cow and a pig
+        With a great big nose
+        This is quite a sight
+        Everybody knows 
+        (x2)
+        
+        It’s Alain, it’s Alain
+        Yes, yes, yes (x2)
+        It’s Alain, it’s Alain
+        With a great big nose
+        (x2)
+        
+        My name is Alain
+        And I have a big nose
+        But I like my nose
+        And that’s the way it goes
+        
+        (Repeat several times)`,
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
     {
         id: 4,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: 'Bones, you must have them',
+        titleF: 'Des os, il en faut',
+        album: 'Parapluie',
+        french: `Tu as deux mains et deux pieds
+        Tu as deux jambes et un nez
+        Tu as un ventre et un dos
+        Et des muscles sous la peau
+        
+        Tu as une tête et un cou
+        Deux oreilles et deux genoux
+        Tu as deux yeux et deux joues
+        Et une bouche qui mange tout
+        
+        Sous ta peau il y a des os
+        Des petits et des gros
+        Des os, des os, il en faut
+        C'est parceque tu as des os que ...
+        
+        (Répétez deux fois)`,
+        english: `You have two hands and two feet
+        You have two legs and a nose
+        You have a stomach and a back
+        And muscles underneath your skin
+        
+        You have a head and a neck
+        Two ears and two knees
+        You have two eyes and two cheeks
+        And a mouth that eats everything
+        
+        Under you skin, there are bones
+        Small ones and big ones
+        Bones, bones, you must have them
+        It's because you have bones that ...
+        
+        (Repeat two Times)`,
+        copyright: '© 2006 Alain Le Lait',
         link: '',
     },
     {
         id: 5,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: 'Hello!',
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `Hello, hello
+        Hey, how are you?
+        Hello, hello
+        Very well, thank you
+        I’m so happy to be here
+        I’m so happy that you’re near
+        Hello, hello
+        Hey, how are you?
+        
+        Thank you, thank you
+        For being here
+        Thank you, thank you
+        Now open up your ears
+        How do you like this little song?
+        Hey why don’t you sing along?
+        Thank you, thank you
+        For being here`,
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
     {
         id: 6,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: 'I Brush My Teeth',
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `Every morning after breakfast
+        I brush my teeth
+        Every evening after dinner
+        I brush my teeth
+        
+        Every day I brush my teeth
+        To keep them nice and white
+        Don’t forget to brush your teeth
+        Before you go to bed at night
+        (x2)
+        
+        Monday, Tuesday, Wednesday
+        Thursday, Friday, Saturday
+        Sunday
+        
+        (Repeat 1st verse)`,
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
     {
         id: 7,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: 'I Have a Car',
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `I have a car
+        It’s red and blue
+        It’s black and white
+        And yellow too
+        I have a car 
+        It’s pink and green
+        It is a fine, a fine machine
+        
+        I have a truck
+        It’s red and blue
+        It’s black and white
+        And yellow too
+        I have a car 
+        It’s pink and green
+        It is a fine, a fine machine
+        
+        I like colors on my car
+        I like colors on my truck
+        I like colors on my duck
+        
+        (x2)`,
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
     {
         id: 8,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: `I'm Hungry`,
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `I’m hungry, I eat
+        I’m thirsty, I drink
+        When I’m sleepy, I sleep
+        (x2)
+        
+        And every day I play, play, play
+        In the playground near my house
+        (x2)
+        
+        I’m hungry, I eat
+        I’m thirsty, I drink
+        When I’m sleepy, I sleep
+        (x2)
+        
+        And every day I run, run, run
+        In the playground near my house
+        (x2)
+        
+        I’m hungry, I eat
+        I’m thirsty, I drink
+        When I’m sleepy, I sleep
+        (x2)
+        
+        And every day I jump, jump, jump
+        In the playground near my house
+        (x2)`,
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
     {
         id: 9,
-        titleE: '',
-        titleF: '',
-        french: '',
-        english: '',
-        copyright: '',
+        image: '',
+        titleE: `It's So Good`,
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `What I like is peanut butter
+        It’s so good, oh it’s so good
+        What I like is peanut butter
+        It’s good but it sticks to my teeth
+        
+        Yum, yum, yum, oh it’s so good
+        It’s good but it sticks to my teeth
+        (x2)
+        
+        What I like is sunny weather
+        It’s so nice, oh it’s so nice
+        What I like is sunny weather
+        I put sunscreen all over me
+        
+        It smells so good, it smells so good
+        When I put sunscreen all over me
+        (x2)`,
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
     {
         id: 10,
+        image: '',
+        titleE: '',
+        titleF: `J'ai un réfrigérateur`,
+        album: 'Parapluie',
+        french: `J'ai un réfrigérateur
+        Je mets le lait et le beurre
+        Dans mon réfrigérateur
+        
+        J'ai une machine à laver
+        Mes habits sales je les mets 
+        Dans la machine à laver
+        
+        Il ne faut pas mettre le lait
+        Dans la machine à laver
+        Il faut le garder 
+        Bien au frais
+        Au frais, au frais, au frais
+        
+        Les habits sales, par malheur
+        Ne les mets pas dans le réfrigérateur
+        Avec le lait et le beurre
+        
+        (Répétez une fois)`,
+        english: `I have a refrigerator
+        I put the milk and the butter
+        In my refrigerator
+        
+        I have a washing machine
+        My dirty clothes I put them
+        In the washing machine
+        
+        You must not put the milk
+        In the washing machine
+        You must keep it
+        Where it's cold
+        It's cold, it's cold, it’s cold
+        
+        The dirty clothes, by golly!
+        Don't put them in the refrigerator
+        With the milk and the butter
+        
+        (Repeat one time)`,
+        copyright: '© 2006 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 11,
+        image: '',
         titleE: '',
         titleF: '',
-        french: '',
-        english: '',
+        album: '',
+        french: ``,
+        english: ``,
         copyright: '',
         link: '',
     },
--- src/pages/Song.js
+++ src/pages/Song.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { songs } from '../MusicData';
 
 const Song = (props) => {
-    const song = songs[0];
+    const song = songs[2];
 
     return (
         <div style={{ width: '300px', whiteSpace: 'pre-line' }}>
             <h2 className="text-xl font-bold mb-2">{song.titleE}</h2>
-            <p>{song.french}</p>
+            <p>{song.english}</p>
         </div>
     )
 }
