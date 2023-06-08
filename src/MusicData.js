a2e6e20bb92f64282d2d631579e21fdd74111178
Sara Le Lait <sjlelait@gmail.com>
2023-06-07 18:24:23 -0600

    wip adds songs 11-20

--- src/MusicData.js
+++ src/MusicData.js
@@ -420,8 +420,8 @@ export const songs = [
     {
         id: 10,
         image: '',
-        titleE: '',
-        titleF: `J'ai un réfrigérateur`,
+        titleE: 'I Have a Refrigerator',
+        titleF: `J'ai un Réfrigérateur`,
         album: 'Parapluie',
         french: `J'ai un réfrigérateur
         Je mets le lait et le beurre
@@ -467,12 +467,675 @@ export const songs = [
     {
         id: 11,
         image: '',
+        titleE: 'The French Alphabet',
+        titleF: `L'alphabet en Français`,
+        album: `C'est Si Bon`,
+        french: `A, B, C, D, E
+
+        F, G, H, I, J
+        
+        K, L, M, N, O
+        
+        P, Q, R, S, T
+        
+        U, V, W, X, Y, Z
+        
+        C’est l’alphabet enFrançais
+        
+        (x3)`,
+        english: `A, B, C, D, E
+
+        F, G, H, I, J
+        
+        K, L, M, N, O
+        
+        P, Q, R, S, T
+        
+        U, V, W, X, Y, Z
+
+        It’s the French alphabet`,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 12,
+        image: '',
+        titleE: 'Recess Time',
+        titleF: `L'heure de la Récréation`,
+        album: `Parapluie`,
+        french: `Il est dix heures
+        Il n'est pas neuf heures
+        Et il n'est pas onze heures
+        Il n'est pas dix heures et demi
+        (x2)
+        
+        C'est l'heure de la récréation
+        Et je vais jouer avec mes copains         
+        Nous avons un ballon
+        Pour jouer au foot
+        Je prétends que je suis Zizou
+        Quand je marque un but
+        But!
+        
+        Il est dix heures
+        Il n'est pas neuf heures
+        Et il n'est pas onze heures
+        Il n'est pas dix heures et demi
+        
+        C'est l'heure de la récréation
+        Et je vais jouer avec mes copains
+        Nous avons un ballon
+        Pour jouer au foot
+        Je prétends que je suis Zizou
+        Quand je marque un but
+        But!
+        
+        Il est dix heures
+        Il n'est pas neuf heures
+        Et il n'est pas onze heures
+        C'est l'heure de la récréation (x3)`,
+        english: `It is ten o'clock
+        It is not nine o'clock
+        And it is not eleven o'clock
+        It is not ten thirty
+         
+        
+        It is recess time
+        And I am going to play with my friends        
+        We have a ball
+        To play soccer with
+        I pretend that I am Zizou
+        When I score a goal
+        Goal!
+        
+        It is ten o'clock
+        It is not nine o'clock
+        And it is not eleven o'clock
+        It is not ten thirty
+        
+        It is recess time
+        And I am going to play with my friends
+        We have a ball
+        To play soccer with
+        I pretend that I am Zizou
+        When I score a goal
+        Goal!
+        
+        It is ten o'clock
+        It is not nine o'clock
+        And it is not eleven o'clock
+        It is recess time`,
+        copyright: '© 2006 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 13,
+        image: '',
+        titleE: 'The Giraffe and the Hippopotamus',
+        titleF: `La Girafe et L’hippopotame`,
+        album: `C'est Si Bon`,
+        french: `Elle a un trés long cou
+        Elle a des gros genoux
+        Elle a des taches partout
+        Elle n’aime pas les hiboux, ouh
+        
+        Lui, il a des grosses dents
+        Lui, il est un peu lent
+        Il se roule dans la boue
+        Tout le temps
+        Et il n’aime pas les hiboux, ouh
+        
+        La girafe et l’hippopotame
+        N’aiment pas les hiboux
+        Mais si il n’y avait pas de hiboux
+        Ce serait triste comme tout
+        
+        (Répétez depuis le début)`,
+        english: `It has a very long neck
+
+        It has big knees
+        It has spots everywhere
+        It doesn’t like owls, ooh
+        
+        Him, it has large teeth
+        Him, it is a little slow
+        It rolls around in the mud
+        All the time
+        And it doesn’t like owls, ooh
+        
+        The girafe and the hippopotamus
+        Don’t like owls
+        But if there weren’t any owls
+        It would be very sad
+        
+        (Repeat from the beginning)`,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 14,
+        image: '',
+        titleE: 'The Fish',
+        titleF: `Les poissons`,
+        album: `Parapluie`,
+        french: `Les poissons, les poissons
+        Ça ne sent pas tellement bon
+        Les poissons, les poissons
+        Ça a les yeux ronds
+        (x2)
+        
+        Les poissons, on, on, on
+        Les poissons, in, in, in
+        Les poissons, on, on, on
+        Ça nage trés bien
+        (x2)
+        
+        Les oiseaux, les oiseaux
+        Ça mange les escargots
+        Les oiseaux, les oiseaux
+        Et ça vole haut
+        (x2)
+        
+        Les oiseaux, o, o, o
+        Les oiseaux, in, in, in
+        Les oiseaux, o, o, o
+        Ça vole trés bien
+        (x2)
+        
+        Des lapins, des lapins
+        Il y en plein mon jardin
+        Des lapins, des lapins
+        Qui mangent les raisins
+        (x2)
+        
+        Les lapins, i, i, i
+        Les lapins, in, in, in
+        Les lapins, i, i, i
+        Ça saute trés bien
+        (x2)`,
+        english: `The fish, the fish
+        They don’t smell so good
+        The fish, the fish
+        They have round eyes
+        (x2)
+        
+        The fish, on, on, on
+        The fish, in, in, in
+        The fish, on, on, on
+        They swim very well
+        (x2)
+        
+        The birds, the birds
+        They eat snails
+        The birds, the birds
+        And they fly high
+        (x2)
+        
+        The birds, o, o, o
+        The birds, in, in, in
+        The birds, o, o, o
+        They fly very high
+        (x2)
+        
+        Rabbits, rabbits
+        My garden is full of them
+        Rabbits, rabbits
+        Eating the grapes
+        (x2)
+        
+        The rabbits, i, i, i
+        The rabbits, in, in, in
+        The rabbits, i, i, i
+        They jump very well
+        (x2)`,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 15,
+        image: '',
+        titleE: 'Why is it making this noise?',
+        titleF: `Pourquoi Fait-il ce Bruit La?`,
+        album: `Parapluie`,
+        french: `Mon estomac fait un bruit étrange
+        Quand je bois du lait ou
+        
+        quand je mange
+        Du gâteau au chocolat,
+        
+        je ne comprends pas
+        
+        Sais-tu pourquoi mon ventre fait ce bruit la?
+        Si tu le sais, s'il te plaît oh dis-le moi
+        Pourquoi fait-il ce bruit la?
+        
+        je ne comprends pas
+        
+        Mon ventre ne devrait pas
+        Faire un bruit comme ça
+        Pourquoi? pourquoi?
+        
+        Mon ventre fait ce bruit la?
+        Je ne comprends pas
+        Chaque fois que je mange
+        Du gâteau au chocolat
+        
+        Pourquoi fait-il ce bruit la?
+        Je ne comprends pas
+        Mon ventre ne devrait pas
+        Faire un bruit comme ça
+        
+        Chaque fois que je mange
+        Du gâteau au chocolat
+        Mon ventre fait ce bruit la
+        Je ne comprends pas`,
+        english: `My stomach makes a funny noise
+        When I drink milk or
+        
+        when I eat
+        Chocolate cake,
+        
+        I don't understand
+        
+        Do you know why my stomach makes this noise?
+        If you do, oh please tell me
+        Why is it making this noise?
+        
+        I don't understand
+        
+        My stomach should not
+        Make a noise like that
+        Why? Why?
+        
+        Is my stomach making this noise?
+        I don't understand
+        Every time I eat
+        Chocolate cake
+        
+        Why is it making this noise?
+        I don't understand
+        My stomach should not 
+        Make a noise like that
+        
+        Every time I eat
+        Chocolate cake
+        My stomach makes this noise
+        I don't understand`,
+        copyright: '© 2006 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 16,
+        image: '',
+        titleE: 'Sing the Alphabet',
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `A, B, C, D, E
+
+        F, G, H, I, J
+        
+        K, L, M, N, O
+        
+        P, Q, R, S, T
+        
+        U, V, W, X, Y, Z
+        
+        Sing the alphabet with me `,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 17,
+        image: '',
+        titleE: 'Sit Down',
+        titleF: null,
+        album: `It's So Good`,
+        french: null,
+        english: `Sit down, stand up
+        Stand up, sit down
+        Come and sit right next to me
+        Would you like a cup of tea?
+        
+        You stand, I sit
+        I sit, you stand
+        You are standing on your feet
+        While I’m sitting in my seat
+        
+        You can sit or you can stand
+        It is up to you my friend
+        But if you want to see me smile
+        Stay here for a while 
+        (x2)`,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 18,
+        image: '',
+        titleE: 'Lark',
+        titleF: `Alouette - CD version`,
+        album: `Parapluie`,
+        french: `Chanson traditionelle
+
+ 
+
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai la tête 
+        Je te plumerai la tête
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o
+        
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai le bec (x2)
+        Et le bec - et le bec
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o
+        
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai le cou (x2)
+        Et le cou - et le cou
+        Et le bec - et le bec
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o
+        
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai les ailes (x2)
+        Et les ailes - et les ailes
+        Et le cou - et le cou
+        Et le bec - et le bec
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o
+        
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai le dos (x2)
+        Et le dos - et le dos
+        Et les ailes - et les ailes
+        Et le cou - et le cou
+        Et le bec - et le bec
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o
+        
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai les pattes (x2)
+        Et les pattes - et les pattes
+        Et le dos - et le dos
+        Et les ailes - et les ailes
+        Et le cou - et le cou
+        Et le bec - et le bec
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o
+        
+        Alouette, gentille alouette
+        Alouette, je te plumerai
+        Je te plumerai la queue (x2)
+        Et la queue - et la queue
+        Et les pattes - et les pattes
+        Et le dos - et le dos
+        Et les ailes - et les ailes
+        Et le cou - et le cou
+        Et le bec - et le bec
+        Et la tête - et la tête
+        Alouette - alouette
+        O, o, o, o, o`,
+        english: `Traditional
+
+ 
+
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your head
+        I will pull the feathers off your head
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o
+        
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your beak
+        And the beak - and the beak
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o
+        
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your neck
+        And the neck - and the neck
+        And the beak - and the beak
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o
+        
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your wings
+        And the wings - and the wings
+        And the neck - and the neck
+        And the beak - and the beak
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o
+        
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your back
+        And the back - and the back
+        And the wings - and the wings
+        And the neck - and the neck
+        And the beak - and the beak
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o
+        
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your legs
+        And the legs - and the legs
+        And the back - and the back
+        And the wings - and the wings
+        And the neck - and the neck
+        And the beak - and the beak
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o
+        
+        Lark, gentle lark
+        Lark, I will pull your feathers off
+        I will pull the feathers off your tail
+        And the tail - and the tail
+        And the legs - and the legs
+        And the back - and the back
+        And the wings - and the wings
+        And the neck - and the neck
+        And the beak - and the beak
+        And the head - and the head
+        Lark, lark
+        O, o, o, o, o`,
+        copyright: '© 2006 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 19,
+        image: '',
+        titleE: 'Sit Down, Stand Up',
+        titleF: `Assis, Debout`,
+        album: `C'est Si Bon`,
+        french: `Assis, debout
+        Debout, assis
+        Assieds-toi à côté de moi
+        Et dis-moi comment tu vas
+        Tu es debout, je suis assis
+        Si tu veux rester debout
+        Ça ne me dérange
+        Pas du tout
+        
+        Assieds-toi à côté de moi
+        Et dis-moi comment tu vas
+        Préfères-tu rester debout
+        Ça ne me dérange
+        Pas du tout
+        (x2)
+        
+        (Répétez depuis le début)`,
+        english: `Sit down, stand up
+        Stand up, sit down
+        Sit down next to me
+        And tell me how you are doing
+        You are standing, I am sitting
+        If you want to stand
+        It doesn’t bother me
+        At all
+        
+        Sit down next to me
+        And tell me how you are doing
+        Would you rather stand
+        It doesn’t bother me 
+        At all
+         
+        
+        (Repeat from the beginning)`,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 20,
+        image: '',
+        titleE: 'With a Big Nose',
+        titleF: `Avec un Gros Nez`,
+        album: `Soyons Amis`,
+        french: `C’est un chien, c’est un chien
+        Ouah, ouah, ouah (répétez)
+        C’est un chien, c’est un chien
+        Avec un gros nez
+        C’est un chat, c’est un chat
+        Miaou, miaou, miaou (répétez)
+        C’est un chat, c’est un chat
+        Avec un gros nez
+        
+        Un chien et un chat, avec un gros nez
+        Ce n’est pas souvent qu’on en voit passer 
+        (x2)
+        
+        Un oiseau, un oiseau
+        (Sifflez 3 fois) répétez
+        Un oiseau, un oiseau
+        Avec un gros nez
+        Un poisson, un poisson
+        (Claquez les lèvres 3 fois) répétez
+        Un poisson, un poisson
+        Avec un gros nez
+        
+        Un oiseau, un poisson, avec un gros nez
+        Ce n’est pas souvent qu’on en voit passer 
+        (x2)
+        
+        C’est une vache, c’est une vache
+        Meuh, meuh (répétez)
+        C’est une vache, c’est une vache 
+        Avec un gros nez
+        Un cochon, un cochon
+        Ron, ron, ron (répétez)
+        Un cochon, un cochon
+        Avec un gros nez
+        
+        Une vache, un cochon, avec un gros nez
+        Ce n’est pas souvent qu’on en voit passer 
+        (x2)
+        
+        C’est Alain, c’est Alain
+        Oui, oui, oui (répétez)
+        C’est Alain, c’est Alain
+        Avec un gros nez
+        (Répétez ce couplet)
+        
+        Je m’appelle Alain
+        Et j’ai un gros nez
+        Mais ca ne fait rien
+        Car mon nez je l’aime bien
+        Il s’appelle Alain
+        Et il a un gros nez
+        Mais ca ne fait rien
+        Car son nez on l’aime bien
+        (Répétez)`,
+        english: `It’s a dog, it’s a dog
+        Woof, woof, woof (repeat)
+        It’s a dog, it’s a dog
+        With a big nose
+        It’s a cat, it’s a cat
+        Meow, meow (repeat)
+        It’s a cat, it’s a cat
+        With a big nose
+        
+        A dog and a cat, with a big nose
+        It is not often that you see that go by 
+        (x2)
+        
+        A bird, a bird
+        (Whistle 3 times) repeat
+        A bird, a bird
+        With a big nose
+        A fish, a fish
+        (Smack lips 3 times) repeat
+        A fish, a fish
+        With a big nose
+        
+        A bird, a fish, with a big nose
+        It is not often that you see that go by 
+        (x2)
+        
+        It’s a cow, it’s a cow
+        Moo, moo (repeat)
+        It’s a cow, it’s a cow
+        With a great big nose
+        A pig, a pig
+        Oink, oink, oink (repeat)
+        A pig, a pig
+        With a big nose
+        
+        A cow, a pig, with a big nose
+        It is not often that you see that go by 
+        (x2)
+        
+        It’s Alain, it’s Alain
+        Yes, yes, yes (repeat)
+        It’s Alain, it’s Alain
+        With a big nose
+        (Repeat this verse)
+        
+        My name is Alain
+        And I have a big nose
+        But it doesn’t matter
+        Because I like my nose
+        His name is Alain
+        And he has a big nose
+        But it doesn’t matter
+        Because we like his nose
+        (Repeat) `,
+        copyright: '© 2003 Alain Le Lait',
+        link: '',
+    },
+    {
+        id: 21,
+        image: '',
         titleE: '',
-        titleF: '',
-        album: '',
+        titleF: ``,
+        album: ``,
         french: ``,
         english: ``,
-        copyright: '',
+        copyright: '© 2003 Alain Le Lait',
         link: '',
     },
 ]
