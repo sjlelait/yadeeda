77532b04e51222133c895351f3b7f53865d8c875
Sara Le Lait <sjlelait@gmail.com>
2023-06-11 17:36:32 -0600

    wip: adds Song.js to render song data

--- src/pages/Song.js
+++ src/pages/Song.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import { songs } from '../MusicData';
 
-const Song = (props) => {
-    const song = songs[2];
+const Song = () => {
+    const { id } = useParams();
+    const song = songs.find((song) => song.id === Number(id));
+
 
     return (
-        <div style={{ width: '300px', whiteSpace: 'pre-line' }}>
-            <h2 className="text-xl font-bold mb-2">{song.titleE}</h2>
-            <p>{song.english}</p>
+        <div className="w-full overflow-auto pb-16">
+            {song.titleF ? (
+            <div className="flex justify-between">
+                <div className="w-1/2 pr-4">
+                <h2>{song.titleF}</h2>
+                <p>{song.copyright}</p>
+                <p>{song.french}</p>
+                </div>
+                <div className="w-1/2 pl-4">
+                <h2>{song.titleE}</h2>
+                {song.english && <p>{song.english}</p>}
+                </div>
+            </div>
+            ) : (
+            <div className="text-center">
+                <h2>{song.titleE}</h2>
+                <p>{song.copyright}</p>
+                {song.english && <p>{song.english}</p>}
+            </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default Song;
