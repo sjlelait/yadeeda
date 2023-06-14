1234a887c9a699165841d90c211cc66c0c803e75
Sara Le Lait <sjlelait@gmail.com>
2023-06-13 18:52:41 -0600

    wip: edits button styling Home

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -2,16 +2,26 @@ import React from 'react';
 
 const HomeCard = ({ title, image, description, link, bgColor }) => {
   return (
-    <div className={`rounded-lg shadow-lg p-4 ${bgColor}`} style={{ width: '300px' }}>
-      {image && (
-        <img src={image} alt={title} className="w-full h-32 object-cover mb-4 rounded" />
-      )}
-      {title && <h2 className="text-xl font-bold mb-2">{title}</h2>}
-      {description && <p>{description}</p>}
-      {link && 
-        <a href={link} className="bg-orange-500 text-white px-4 py-2 mt-4 inline-block rounded">
-          Let's Go!
-        </a>}
+    <div className={`rounded-lg shadow-lg p-4 m-5 ${bgColor} flex flex-wrap md:flex-no-wrap`} style={{ width: '300px' }}>
+      <div className="flex flex-col h-full">
+        {image && (
+          <img src={image} alt={title} className="w-full h-32 object-cover mb-4 rounded" />
+        )}
+        <div className="flex flex-col justify-between flex-grow">
+          <div>
+            {title && <h2 className="text-xl font-bold mb-2">{title}</h2>}
+            {description && <p>{description}</p>}
+          </div>
+          {link && (
+            <a
+              href={link}
+              className="bg-orange-500 text-white py-2 mt-5 inline-block rounded"
+            >
+              Let's Go!
+            </a>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,26 +4,26 @@ import HomeCard from '../components/HomeCard';
 const Home = (props) => {
     const cardData = [
         {
-          title: '10 Reasons to use our songs',
-          image: null,
-          description: '1. one of the best ways to introduce a new language',
-          link: 'https://example.com/card1',
-          bgColor: 'bg-red-500',
-        },
-        {
           title: 'Video of the Day',
           image: null,
           description: 'Random Video Pulled from Youtube, embedded ',
           link: 'https://www.youtube.com/@tialela99',
-          bgColor: 'bg-yellow-500',
+          bgColor: 'bg-red-500',
         },
         {
           title: 'Sesame Street',
           image: null,
           description: 'Yadeeda is on Sesame Street! Alain Le Lait\'s popular song "Sit Down, Stand Up" can now be heard on episode #4135 (The Bookaneers), the French version "Assis, Debout" is in episode #4151 (Slimey competes for the Worm Cup).',
           link: 'https://example.com/card1',
-          bgColor: 'bg-green-500',
+          bgColor: 'bg-yellow-500',
         },
+        {
+            title: '10 Reasons to use our songs',
+            image: null,
+            description: '1. one of the best ways to introduce a new language',
+            link: 'https://example.com/card1',
+            bgColor: 'bg-green-500',
+          },
       ];
 
     return (
