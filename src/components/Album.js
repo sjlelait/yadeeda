a883fa2409660ff92dee37fac05e427d4436b081
Sara Le Lait <sjlelait@gmail.com>
2023-06-27 18:13:51 -0600

    wip: adds Album.js for filtering songs

---
+++ src/components/Album.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Album = ({ album, selectedAlbum, setSelectedAlbum }) => {
+  const handleAlbumChange = (event) => {
+    const album = event.target.value;
+    setSelectedAlbum(album);
+  };
+
+  return (
+    <div className="flex flex-col items-center mt-2 mb-5">
+        <div className="mb-1">
+            <h2 className="text-lg font-bold">Albums:</h2>
+        </div>
+        <div className="border border-gray-300 rounded-lg overflow-hidden">
+            <select value={selectedAlbum} onChange={handleAlbumChange}>
+                <option value="">All Songs</option>
+                {album.map((album, index) => (
+                <option key={index} value={album}>
+                    {album}
+                </option>
+                ))}
+            </select>
+        </div>
+    </div>
+  );
+};
+
+export default Album;
--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SongList from '../components/SongList';
 import {songs} from '../MusicData';
+import Album from '../components/Album';
 
 const Songs = (props) => {
+    const [selectedAlbum, setSelectedAlbum] = useState(null);
+
+    const filteredSongs = selectedAlbum
+        ? songs.filter((song) => {
+            if (Array.isArray(song.album)) {
+            return song.album.some((album) => album === selectedAlbum);
+            } else {
+            return song.album === selectedAlbum;
+            }
+        })
+        : songs;
+
+    const album = [...new Set(songs.flatMap((song) => {
+        if (Array.isArray(song.album)) {
+        return song.album;
+        } else {
+        return [song.album];
+        }
+    }))];
+
     return (
     <div>
-        <h1>Yadeeda Songs</h1>
-        <SongList songs={songs} />
+        <h1>Yadeeda Songs from Albums</h1>
+        <Album
+            album={album}
+            selectedAlbum={selectedAlbum}
+            setSelectedAlbum={setSelectedAlbum}
+        />
+        <SongList songs={filteredSongs} />
     </div>
     );
 };
