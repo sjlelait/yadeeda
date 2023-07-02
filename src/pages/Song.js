446cfcc11faeab3c049ce53ad325811ebaaa39f0
Sara Le Lait <sjlelait@gmail.com>
2023-07-02 17:22:51 -0600

    wip: adds text for english/french lyrics

--- src/pages/Song.js
+++ src/pages/Song.js
@@ -8,20 +8,25 @@ const Song = () => {
 
 
     return (
-        <div className="w-full overflow-auto pb-16">
+        <div className="w-full overflow-auto p-16 text-left">
+            <div className="flex items-center items-start">
+                <img src={song.image} alt="Yadeeda Album" className="w-32 h-auto md:w-48 mb-6" />
+            </div>
             {song.titleF ? (
             <div className="flex justify-between">
-                <div className="w-1/2 pr-4">
-                <h1 className="text-xl font-bold">{song.titleF}</h1>
-                <p className="text-gray-500">{song.copyright}</p>
-                <br />
-                <p style={{ whiteSpace: 'pre-line' }}>{song.french}</p>
+                <div className="w-1/2 pl-12">
+                    <h1 className="text-xl font-bold">{song.titleF}</h1>
+                    <p className="text-gray-700">(French Lyrics)</p>
+                    <p className="text-gray-500">{song.copyright}</p>
+                    <br />
+                    <p style={{ whiteSpace: 'pre-line' }}>{song.french}</p>
                 </div>
-                <div className="w-1/2 pl-4">
-                <h1 className="text-xl font-bold">{song.titleE}</h1>
-                <p className="text-gray-500">{song.copyright}</p>
-                <br />
-                {song.english && <p style={{ whiteSpace: 'pre-line' }}>{song.english}</p>}
+                <div className="w-1/2 pl-12">
+                    <h1 className="text-xl font-bold">{song.titleE}</h1>
+                    <p className="text-gray-700">(English Lyrics)</p>
+                    <p className="text-gray-500">{song.copyright}</p>
+                    <br />
+                    {song.english && <p style={{ whiteSpace: 'pre-line' }}>{song.english}</p>}
                 </div>
             </div>
             ) : (
