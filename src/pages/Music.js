3a9c5a7d3f098a9cab129f901c4203fd6cb21ae0
Sara Le Lait <sjlelait@gmail.com>
2023-07-02 18:37:11 -0600

    wip: edits styling for music

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -3,25 +3,26 @@ import { Link } from 'react-router-dom';
 import SongsFromVideosCover from '../assets/SongsFromVideosCover.jpg';
 import BigNose from '../assets/BigNose.jpg';
 import CestSiBon from '../assets/CestSiBonCover.jpg';
+import Espadrilles from '../assets/EspadrillesCover.jpg';
 
 const Music = (props) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-14">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-14 md:mt-20 md:gap-x-0">
             <Link to="/videos" className="flex flex-col items-center mb-4">
-                <img src={BigNose} alt='Videos' className="w-32 h-32 object-cover mb-2 rounded" />
+                <img src={BigNose} alt='Videos' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
                 <h3 className="text-lg font-semibold">Videos</h3>
             </Link>
             <Link to="/songs" className="flex flex-col items-center mb-4">
-                <img src={CestSiBon} alt='Individual Songs' className="w-32 h-32 object-cover mb-2 rounded" />
+                <img src={CestSiBon} alt='Individual Songs' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
                 <h3 className="text-lg font-semibold">Music (Kids/Teaching)</h3>
             </Link>
             <Link to="" className="flex flex-col items-center mb-4">
-                <img src={SongsFromVideosCover} alt='Songs From Videos' className="w-32 h-32 object-cover mb-2 rounded" />
+                <img src={SongsFromVideosCover} alt='Songs From Videos' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
                 <h3 className="text-lg font-semibold">Songs From Videos</h3>
             </Link>
             <Link to="" className="flex flex-col items-center mb-4">
-                <img src='' alt='Espadrilles' className="w-32 h-32 object-cover mb-2 rounded" />
-                <h3 className="text-lg font-semibold">Espadrilles - Adult Music</h3>
+                <img src={Espadrilles} alt='Espadrilles' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
+                <h3 className="text-lg font-semibold">Adult Music</h3>
             </Link>
         </div>
     )
--- src/pages/Song.js
+++ src/pages/Song.js
@@ -8,36 +8,37 @@ const Song = () => {
 
 
     return (
-        <div className="w-full overflow-auto p-16 text-left">
-            <div className="flex items-center items-start">
-                <img src={song.image} alt="Yadeeda Album" className="w-32 h-auto md:w-48 mb-6" />
-            </div>
-            {song.titleF ? (
-            <div className="flex justify-between">
-                <div className="w-1/2 pl-12">
-                    <h1 className="text-xl font-bold">{song.titleF}</h1>
-                    <p className="text-gray-700">(French Lyrics)</p>
-                    <p className="text-gray-500">{song.copyright}</p>
-                    <br />
-                    <p style={{ whiteSpace: 'pre-line' }}>{song.french}</p>
+        <div className="w-full overflow-auto text-left mt-16 md:p-16">
+            <div className="flex flex-col md:flex-row">
+                <img src={song.image} alt="Yadeeda Album" className="w-32 h-32 mr-6" />
+                {song.titleF ? (
+                <div className="mt-10 md:flex md:flex-row md:items-start">
+                    <div className="md:w-1/2 pr-4">
+                        <h1 className="text-xl font-bold">{song.titleF}</h1>
+                        <p className="text-gray-700">(French Lyrics)</p>
+                        <p className="text-gray-500">{song.copyright}</p>
+                        <br />
+                        <p style={{ whiteSpace: 'pre-line' }}>{song.french}</p>
+                    </div>
+                    <div className="w-1/2 mt-12 md:pl-12 md:mt-0">
+                        <h1 className="text-xl font-bold">{song.titleE}</h1>
+                        <p className="text-gray-700">(English Lyrics)</p>
+                        <p className="text-gray-500">{song.copyright}</p>
+                        <br />
+                        {song.english && <p style={{ whiteSpace: 'pre-line' }}>{song.english}</p>}
+                    </div>
                 </div>
-                <div className="w-1/2 pl-12">
+                ) : (
+                <div className="text-left">
                     <h1 className="text-xl font-bold">{song.titleE}</h1>
                     <p className="text-gray-700">(English Lyrics)</p>
                     <p className="text-gray-500">{song.copyright}</p>
                     <br />
                     {song.english && <p style={{ whiteSpace: 'pre-line' }}>{song.english}</p>}
                 </div>
+                )}
             </div>
-            ) : (
-            <div className="text-center">
-                <h1 className="text-xl font-bold">{song.titleE}</h1>
-                <p className="text-gray-500">{song.copyright}</p>
-                <br />
-                {song.english && <p style={{ whiteSpace: 'pre-line' }}>{song.english}</p>}
             </div>
-            )}
-        </div>
     );
 };
 
