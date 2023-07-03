f59c68501c89b3b70a336fc1cb705669bf9b2469
Sara Le Lait <sjlelait@gmail.com>
2023-07-03 16:12:55 -0600

    wip: edits music.js spacing

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -7,23 +7,26 @@ import Espadrilles from '../assets/EspadrillesCover.jpg';
 
 const Music = (props) => {
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-14 md:mt-20 md:gap-x-0">
-            <Link to="/videos" className="flex flex-col items-center mb-4">
-                <img src={BigNose} alt='Videos' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
-                <h3 className="text-lg font-semibold">Videos</h3>
-            </Link>
-            <Link to="/songs" className="flex flex-col items-center mb-4">
-                <img src={CestSiBon} alt='Individual Songs' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
-                <h3 className="text-lg font-semibold">Music (Kids/Teaching)</h3>
-            </Link>
-            <Link to="" className="flex flex-col items-center mb-4">
-                <img src={SongsFromVideosCover} alt='Songs From Videos' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
-                <h3 className="text-lg font-semibold">Songs From Videos</h3>
-            </Link>
-            <Link to="" className="flex flex-col items-center mb-4">
-                <img src={Espadrilles} alt='Espadrilles' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
-                <h3 className="text-lg font-semibold">Adult Music</h3>
-            </Link>
+        <div>
+            <h1 className="text-lg mt-14 md:text-xl lg:text-3xl font-bold font-homeP text-ydPurple">Music & Videos</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 md:mt-38 gap-4 md:mt-20 md:gap-x-0">
+                <Link to="/videos" className="flex flex-col items-center mb-4">
+                    <img src={BigNose} alt='Videos' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
+                    <h3 className="text-lg font-semibold">Videos</h3>
+                </Link>
+                <Link to="/songs" className="flex flex-col items-center mb-4">
+                    <img src={CestSiBon} alt='Individual Songs' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
+                    <h3 className="text-lg font-semibold">Music (Kids/Teaching)</h3>
+                </Link>
+                <Link to="" className="flex flex-col items-center mb-4">
+                    <img src={SongsFromVideosCover} alt='Songs From Videos' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
+                    <h3 className="text-lg font-semibold">Songs From Videos</h3>
+                </Link>
+                <Link to="" className="flex flex-col items-center mb-4">
+                    <img src={Espadrilles} alt='Espadrilles' className="w-32 h-32 object-cover mb-2 md:w-48 md:h-48 rounded" />
+                    <h3 className="text-lg font-semibold">Adult Music</h3>
+                </Link>
+            </div>
         </div>
     )
 }
