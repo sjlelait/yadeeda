f295496d4cc5ddb7fc1e2175d7ee9df2d421d929
Sara Le Lait <sjlelait@gmail.com>
2023-07-06 17:59:23 -0600

    wip: edits styling for footer/header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
 
 const Footer = (props) => {
+    const location = useLocation();
+    const isHomePage = location.pathname === '/';
+
     return (
-        <footer className="fixed bottom-0 left-0 right-0 w-full text-center justify-between">
+        <footer className={`fixed bottom-0 left-0 right-0 h-16 w-full text-center justify-between bg-ydLightBlue ${isHomePage ? "" : "sticky"}`}>
             <div className="container mx-auto px-4 lg:px-8">
-                <div className="flex flex-col items-center justify-center p-6 dark:border-neutral-500 lg:justify-between">
+                <div className="flex flex-col items-center justify-center p-2 dark:border-neutral-500 lg:justify-between">
                     <div className="flex justify-center flex-wrap md-2">
-                        <Link to="/" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">HOME</Link>
-                        <Link to="/about" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">ABOUT</Link>
-                        <Link to="/reviews" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">REVIEWS</Link>
-                        <Link to="/contact" className="mr-3 text-sm md:text-lg text-neutral-600 dark:text-neutral-400">CONTACT</Link>
+                        <Link to="/" className="mr-3 text-sm md:text-lg text-neutral-600 text-white">HOME</Link>
+                        <Link to="/about" className="mr-3 text-sm md:text-lg text-neutral-600 text-white">ABOUT</Link>
+                        <Link to="/reviews" className="mr-3 text-sm md:text-lg text-neutral-600 text-white">REVIEWS</Link>
+                        <Link to="/contact" className="mr-3 text-sm md:text-lg text-neutral-600 text-white">CONTACT</Link>
                     </div>
-                    <div className="text-center mt-4 lg:mt-0">
+                    <div className="text-center">
                         <p className="text-sm">© Yadeeda { new Date().getFullYear() }</p>
                     </div>
                 </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,9 @@ const Header = (props) => {
 
     return (
         <header className="bg-ydLightBlue">
-            <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-6" aria-label="Global">
+            <nav className="mx-auto flex max-w-7xl items-center justify-between h-16 p-6 lg:px-6 shadow" aria-label="Global">
                 <div className="flex lg:flex-1">
-                    <Link to="/" className="-m-1.5 p-1.5 font-homeP text-xl">
+                    <Link to="/" className="-m-1.5 p-1.5 text-xl font-homeP">
                         <span className="sr-only">Yadeeda</span>
                         Yadeeda
                     </Link>
@@ -92,15 +92,15 @@ const Header = (props) => {
                     onClose={() => setMobileMenuOpen(false)}
                 >
                     <div className="fixed inset-0 z-10" />
-                    <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <Dialog.Panel className="fixed inset-y-0 right-0 z-10 max-w-xs bg-ydLightBlue px-6 py-6 shadow">
                         <div className="flex items-center justify-between">
-                            <Link to="/" className="-m-1.5 p-1.5">
+                            <Link to="/" className="-m-1.5 p-1.5 font-homeP text-lg">
                                 <span className="sr-only">Yadeeda</span>
                                 Yadeeda
                             </Link>
                             <button
                                 type="button"
-                                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                                className="-m-2.5 rounded-md p-2.5"
                                 onClick={() => setMobileMenuOpen(false)}
                             >
                             <span className="sr-only">Close menu</span>
