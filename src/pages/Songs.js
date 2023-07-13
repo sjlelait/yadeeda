2576b8cbf98d4e523cdc1c44f32be282f2d81030
Sara Le Lait <sjlelait@gmail.com>
2023-07-12 19:00:47 -0600

    wip: edits styling

--- src/pages/Songs.js
+++ src/pages/Songs.js
@@ -4,7 +4,6 @@ import {songs} from '../MusicData';
 import PurchaseButtons from '../components/PurchaseButtons';
 
 const Songs = () => {
-    // Group songs by album
     const albums = songs.reduce((acc, song) => {
         const { album, image, copyright } = song;
         if (!acc[album]) {
@@ -16,18 +15,15 @@ const Songs = () => {
   
     return (
         <div>
-            <h1 className="text-xl mt-24 text-2xl lg:mt-16 lg:mb-8 lg:text-3xl font-bold font-homeP underline text-ydPurple">Yadeeda Songs</h1>
-            <div className="grid grid-cols-2 gap-4">
+            <h1 className="text-2xl mt-24 mb-8 text-2xl md:mt-12 md:text-3xl font-bold font-homeP underline text-ydPurple">Yadeeda Albums</h1>
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-y-8">
             {Object.entries(albums).map(([album, { image, copyright, title, songs }], index) => (
                 <div key={index} className="flex flex-col items-center mb-8">
-                    {/* Display album image */}
+                    <h1 className="text-2xl font-semibold mb-2">{title}</h1>
                     <img src={image} alt={title} className="w-32 h-32 object-cover mb-2 rounded" />
-                    {/* Display album title */}
-                    <h3 className="text-lg font-semibold mb-2">{title}</h3>
-                    <h3 className="text-md font-semibold mb-2">{copyright}</h3>
-                    {/* Display songs from the album */}
+                    <h3 className="text-md text-gray-600 mb-6">{copyright}</h3>
                     {songs.map((song) => (
-                        <Link to={`/song/${song.id}`} key={song.id} className="text-base mb-1">
+                        <Link to={`/song/${song.id}`} key={song.id} className="text-base hover:text-ydBlue underline mb-1">
                         {song.titleF || song.titleE}
                         </Link>
                     ))}
