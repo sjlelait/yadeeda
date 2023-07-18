9aede082074306a85edff896dd7ad5cd33cde25b
Sara Le Lait <sjlelait@gmail.com>
2023-07-18 17:17:32 -0600

    test: /songs test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,7 +36,7 @@ const Home = (props) => {
             title: 'All Song Lyrics Here',
             image: Kids,
             description: 'Lyrics in French and English, Makes learning fun!',
-            link: "/songs",
+            link: 'https://yadeeda.netlify.app/songs',
             bgColor: 'bg-ydGreen',
         },
     ];
