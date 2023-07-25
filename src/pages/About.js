511a6a299fe821eac8ddf50b86453cacd7e0ab2b
Sara Le Lait <sjlelait@gmail.com>
2023-07-25 17:16:05 -0600

    wip: edits about page styling & content

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,16 +4,16 @@ import Alain from '../assets/Alain.png';
 const About = (props) => {
     return (
         <div>
-            <h1 className="mb-3 mt-16 text-lg md:text-xl lg:text-3xl font-bold font-homeP text-ydBlue">Yadeeda and Alain Le Lait</h1>
+            <h1 className="bg-white rounded-lg shadow-lg inline-block p-2 md:p-4 mt-8 md:mt-16 text-2xl lg:text-3xl font-bold font-homeP text-ydPurple">Yadeeda and Alain Le Lait</h1>
             <div className="flex flex-col md:flex-row">
-                <div className="m-6 rounded-lg shadow-lg p-4 text-center bg-white md:w-1/2">
+                <div className="p-4 m-2 rounded-lg shadow-lg text-center bg-white md:w-1/2 font-homeP">
                     <div className="flex flex-col items-center justify-center">
                         <img src={Alain} alt="Alain Le Lait" className="h-52 w-auto rounded shadow" />
-                        <p>About Alain</p>
+                        <p className="mt-4">Alain Le Lait has been writing and recording children's music since 1990. His songs and videos are in use in classrooms throughout the world as teaching materials for both kids and adults. You'll find these whymsical and catchy tunes in your head for years to come, reminding you of the language and helping you learn.</p>
                     </div>
                 </div>
-                <div className="m-6 p-4 rounded-lg shadow-lg bg-white md:w-1/2 sm:mt-4 font-homeP">
-                    <h3 className="text-lg font-bold text-ydPurple mb-6 underline lg:text-xl">Top 10 Reasons to use our Songs and Videos</h3>
+                <div className="p-4 m-2 rounded-lg shadow-lg bg-white md:w-1/2 font-homeP">
+                    <h3 className="text-xl font-bold text-ydPurple mb-6 underline lg:text-xl">Top 10 Reasons to use our Songs and Videos</h3>
                     <ul className="text-left">
                         <li className="mb-4 mt-2">1.  One of the best ways to introduce kids to a new language</li>
                         <li className="mb-4">2.  ‘Fun and Enjoyable tunes’ (School Library Journal)</li>
