54531e2bcdfcaf31762b29c633c79bebe57473e2
Sara Le Lait <sjlelait@gmail.com>
2023-07-27 15:04:30 -0600

    wip: edits Home for video of the day functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import HomeCard from '../components/HomeCard';
 import { videos } from '../MusicData';
 import SesameStreet from '../assets/SesameStreet.png';
@@ -9,39 +9,68 @@ const Home = (props) => {
     const filteredVideos = videos.filter(
         (video) => video.category === 'French' || video.category === 'English'
     );
-
-    const getRandomVideo = () => {
-        const randomIndex = Math.floor(Math.random() * filteredVideos.length);
-        return filteredVideos[randomIndex]?.link || '';
-      };
     
-      
+    const getRandomVideoIndex = useCallback(() => {
+        return Math.floor(Math.random() * filteredVideos.length);
+    }, [filteredVideos.length]);
+    
+    const [videoIndex, setVideoIndex] = useState(getRandomVideoIndex);
+    
+    useEffect(() => {
+        const storedIndex = localStorage.getItem('videoIndex');
+        const currentDate = new Date();
+        const storedDate = localStorage.getItem('selectedDate');
+    
+        if (storedIndex !== null && storedDate !== null) {
+          const dayDiff = Math.floor(
+            (currentDate - new Date(storedDate)) / (1000 * 60 * 60 * 24)
+        );
+    
+        if (dayDiff >= 1) {
+            const newIndex = getRandomVideoIndex();
+            setVideoIndex(newIndex);
+            localStorage.setItem('videoIndex', newIndex);
+            localStorage.setItem('selectedDate', currentDate);
+        } else {
+            setVideoIndex(parseInt(storedIndex));
+        }
+        } else {
+          const newIndex = getRandomVideoIndex();
+          setVideoIndex(newIndex);
+          localStorage.setItem('videoIndex', newIndex);
+          localStorage.setItem('selectedDate', currentDate);
+        }
+    }, [getRandomVideoIndex, filteredVideos.length]);
+    
+    const selectedVideo = filteredVideos[videoIndex]?.link || '';
+    
     const cardData = [
         {
-            title: 'Video of the Day',
-            image: null,
-            description: 'Click below to visit our YouTube Channel for more!',
-            link: 'https://www.youtube.com/@tialela99',
-            video: getRandomVideo(),
-            bgColor: 'bg-ydRed',
+          title: 'Video of the Day',
+          image: null,
+          description: 'Click below to visit our YouTube Channel for more!',
+          link: 'https://www.youtube.com/@tialela99',
+          video: selectedVideo,
+          bgColor: 'bg-ydRed',
         },
         {
-            title: null,
-            image: SesameStreet,
-            description: 'Yadeeda is on Sesame Street! Alain Le Lait\'s popular song "Sit Down, Stand Up" can now be heard on episode #4135 (The Bookaneers), the French version "Assis, Debout" is in episode #4151 (Slimey competes for the Worm Cup).',
-            link: 'https://muppet.fandom.com/wiki/Episode_4135',
-            bgColor: 'bg-ydYellow',
+          title: null,
+          image: SesameStreet,
+          description:
+            "Yadeeda is on Sesame Street! Alain Le Lait's popular song 'Sit Down, Stand Up' can now be heard on episode #4135 (The Bookaneers), the French version 'Assis, Debout' is in episode #4151 (Slimey competes for the Worm Cup).",
+          link: 'https://muppet.fandom.com/wiki/Episode_4135',
+          bgColor: 'bg-ydYellow',
         },
         {
-            title: 'All Song Lyrics Here',
-            image: Kids,
-            description: 'Lyrics in French and English, Makes learning fun!',
-            link: '/songs',
-            bgColor: 'bg-ydGreen',
+          title: 'All Song Lyrics Here',
+          image: Kids,
+          description: 'Lyrics in French and English, Makes learning fun!',
+          link: '/songs',
+          bgColor: 'bg-ydGreen',
         },
     ];
-        
-    cardData[0].videoLink = getRandomVideo();  
+    
+    cardData[0].videoLink = selectedVideo;  
         
     return (
         <div className="flex flex-col items-center bg-white">
@@ -61,7 +90,7 @@ const Home = (props) => {
                         />
                     ))}
                 </div>
-            </div>
+        </div>
     )
 }
 
